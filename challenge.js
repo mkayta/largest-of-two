d54c04f4b27a9565162a7cfff02bfe34bc4fa4e6
Eng-Mohamed <mkayta2023@gmail.com>
2024-09-07 19:35:01 +0300

    I done this exercise

--- challenge.js
+++ challenge.js
@@ -1,2 +1,11 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+let num1 = 30; 
+let num2 = 20; 
+if (num1 > num2) {
+    console.log(`The largest number out of the two numbers is ${num1}`);
+} else if (num2 > num1) {
+    console.log(`The largest number out of the two numbers is ${num2}`);
+}
+ let name="Mohamed Hassan"
+ console.log(name)
